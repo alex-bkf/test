657a44442c1cc64ba62faf5fe3ce06838b51ad6f
Bokov Alex <bokov.cyber@gmail.com>
2024-05-27 02:13:36 +0300

    Test commit

---
+++ test2.py
@@ -0,0 +1,29 @@
+a = 'aaf'
+fl = False
+
+for i in a:
+    a = a[-1] + a[:-1]
+    if a == a[::-1]:
+        fl = True
+        break
+
+if fl:
+    print('Ok')
+else:
+    print('No')
+
+fl = False
+b = set(a)
+count = 0
+for i in b:
+    if a.count(i) % 2 == 1:
+        count += 1
+        if count > 1:
+            fl = True
+            break
+if fl:
+    print('No')
+else:
+    print('Ok')
+
+
