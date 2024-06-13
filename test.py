8b7a4efbe61091858273741cdeeb33349ab44367
Alex_Bkv <bokov.cyber@gmail.com>
2024-06-13 18:57:12 +0300

    commit in test2 branch

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ a = 'D:\Skillbox\Git\AIPAOSIDF'
 res = a.replace('\\', '/')
 print(res)
 print(5 ** 5)
-# new code in test2 branch 6 string
+# new code in test2 branch 6 string dsafsdf
 print something new
